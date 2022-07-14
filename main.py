43c52055682b3f2de6e19b4552d912813ca14c24
vonbluhm <yvieharper@protonmail.com>
2022-07-14 22:04:21 +0200

    This is the first version. It still needs some checks of indices to be implemented

---
+++ main.py
@@ -0,0 +1,64 @@
+def print_grid(grid_):
+    output = "  0 1 2\n0"
+    for m in range(3):
+        output += " " + grid_[m]
+    output += "\n1"
+    for m in range(3, 6):
+        output += " " + grid_[m]
+    output += "\n2"
+    for m in range(6, 9):
+        output += " " + grid_[m]
+    return print(output)
+
+
+def victory(grid_, player):
+    for i in range(3):
+        if grid_[i] == grid_[i+3] == grid_[i+6] == player:
+            return True
+    for i in range(0, 7, 3):
+        if grid_[i] == grid_[i + 1] == grid_[i + 2] == player:
+            return True
+    if grid_[0] == grid_[4] == grid_[8] == player:
+        return True
+    if grid_[2] == grid_[4] == grid_[6] == player:
+        return True
+    else: return False
+
+
+def move(grid_, player):
+    print_grid(grid_)
+    print("Player", player + ", make your move!")
+    invalid_move = True
+    while invalid_move:
+        i = input("Row number: ")
+        j = input("Column number: ")
+        if grid_[int(i)*3+int(j)] != "-":
+            print("Invalid move!")
+        else:
+            grid_[int(i)*3+int(j)] = player
+            invalid_move = False
+    return print_grid(grid_)
+
+
+keep_playing = True
+while keep_playing:
+    grid = ["-" for i in range(9)]
+    turn_counter = 0
+    while turn_counter < 9:
+        print("Move", turn_counter+1)
+        if turn_counter % 2:
+            move(grid, "o")
+            turn_counter += 1
+            if victory(grid, "o"):
+                print("Naughts win!")
+                break
+        else:
+            move(grid, "x")
+            turn_counter += 1
+            if victory(grid, "x"):
+                print("Crosses win!")
+                break
+    if turn_counter == 9: print("Draw game!")
+    N = input("Enter N to stop playing, any other character to try again!")
+    if str.lower(N) == "n":
+        keep_playing = False
